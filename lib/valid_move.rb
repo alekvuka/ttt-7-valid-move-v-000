9051e809f76d4807dee76a61fc817a10d17a40dc
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-07 23:54:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,33 @@
 # code your #valid_move? method here
 
+def valid_move(board, index)
+
+  new_index = index.to_i - 1 
+
+  if position_taken(board,new_index) == true 
+    occupied = true 
+  else 
+    occupied = false
+  end 
+
+  if 0 <= new_index <= 9
+    within_index = true
+  else 
+    within_index = false 
+  end
+
+  if occupied == false && within_index == true 
+    return true 
+  else 
+    return false 
+  end 
+end   
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken(board, new_index)
+  if boad[new_index] == "X" || "O"
+    return true 
+  else 
+    return false 
+  end 
+end 
